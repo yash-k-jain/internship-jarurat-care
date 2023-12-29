bb2af8cb193f73ecfb495dc926b72c8c7cd721a1
yash-k-jain <107643838+yash-k-jain@users.noreply.github.com>
2023-12-29 19:29:12 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 
 // db = process.env.MONGO_URI
-db = "mongodb+srv://yashjain7139:yashjain7139@inotebook-cluster.qc2bwpa.mongodb.net/inotebook?retryWrites=true&w=majority"
+// db = "mongodb+srv://yashjain7139:yashjain7139@inotebook-cluster.qc2bwpa.mongodb.net/inotebook?retryWrites=true&w=majority"
+db = "mongodb+srv://yashjain7139:yashjain7139@inotebook-cluster.zcakpdw.mongodb.net/inotebook?retryWrites=true&w=majority"
 
 const connectToMongo = () => {
     mongoose.connect(db, { useNewUrlParser: true })
